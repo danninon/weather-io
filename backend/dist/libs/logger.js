221b3743ffc0601b5ea8c6918e2d6843e9912432
Dan Inon <76201610+danninon@users.noreply.github.com>
2024-09-07 19:48:41 +0300

    Delete backend/dist directory

--- backend/dist/libs/logger.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const loggerConfig_1 = __importDefault(require("../config/loggerConfig"));
-const logger = loggerConfig_1.default.getLogger();
-exports.default = logger;
